449e0407149eb241e935ebefcb9367643246b767
David Tan <davidtansf@gmail.com>
2015-02-13 14:23:00 -0800

    added _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -87,9 +87,22 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var result = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (!test(collection[i])) {
+          result.push(collection[i]);
+      }
+    }
+    return result;
   };
+
+  /* Couldn't find solution using _.filter
+  _.reject = function(collection, test) {
+ // test = ..some code
+};
+  return filter(collection, test);    
+};
+*/
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
