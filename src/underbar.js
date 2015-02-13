9ede58f924bdf230332d6429dc3d0f480a13dcc3
David Tan <davidtansf@gmail.com>
2015-02-13 15:49:32 -0800

    added _.reduce

--- src/underbar.js
+++ src/underbar.js
@@ -159,7 +159,14 @@
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
-  _.reduce = function(collection, iterator, accumulator) {
+  _.reduce = function(collection, iterator, accumulator) { 
+    if (arguments.length !== 3) {
+      accumulator = collection.shift();
+    }
+    for (var i = 0; i < collection.length; i++) {
+      accumulator = iterator(accumulator, collection[i]);
+    }
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
